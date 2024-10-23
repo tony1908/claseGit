239099343e5e344d5114e79aaec11b52358854b6
neucast <luisgneumannc@hotmail.com>
2024-10-23 10:54:45 -0600

    Fixed a security issue.

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
 input("User name: ", userName)
 input("Password: ", password)
 console.log("Your user name is: ", username)
-console.log("Your password is: ", password)
+// console.log("Your password is: ", password)
